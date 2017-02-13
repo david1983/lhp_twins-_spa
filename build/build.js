f59e699af13fedb391cc3bac655861005ac02d71
D4v1d3 <y37arf68>
2017-02-13 12:12:04 +0000

    scaffold-project

--- build/build.js
+++ build/build.js
@@ -7,35 +7,50 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
 
-var s3 = require('s3');
 
-var client = s3.createClient({
-  maxAsyncS3: 20,     // this is the default
-  s3RetryCount: 3,    // this is the default
-  s3RetryDelay: 1000, // this is the default
-  multipartUploadThreshold: 20971520, // this is the default (20 MB)
-  multipartUploadSize: 15728640, // this is the default (15 MB)
-  s3Options: {
-    accessKeyId: "AKIAIXD7LMRIVZRJZJJA",
-    secretAccessKey: "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",
-    // any other options are passed to new AWS.S3()
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
-  },
-});
 
-var params = {
-  localDir: "./dist",
-  deleteRemoved: true, // default false, whether to remove s3 objects
-                       // that have no corresponding local file.
+function uploadOnS3(){
+  var s3 = require('s3');
 
-  s3Params: {
-    Bucket: "lph-twins",
-    Prefix: "",
-    // other options supported by putObject, except Body and ContentLength.
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-  },
-};
+  var client = s3.createClient({
+    maxAsyncS3: 20,     // this is the default
+    s3RetryCount: 3,    // this is the default
+    s3RetryDelay: 1000, // this is the default
+    multipartUploadThreshold: 20971520, // this is the default (20 MB)
+    multipartUploadSize: 15728640, // this is the default (15 MB)
+    s3Options: {
+      accessKeyId: "AKIAIXD7LMRIVZRJZJJA",
+      secretAccessKey: "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",
+      // any other options are passed to new AWS.S3()
+      // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+    },
+  });
 
+  var params = {
+    localDir: "./dist",
+    deleteRemoved: true, // default false, whether to remove s3 objects
+                         // that have no corresponding local file.
+
+    s3Params: {
+      Bucket: "lph-twins",
+      Prefix: "",
+      // other options supported by putObject, except Body and ContentLength.
+      // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+    },
+  };
+
+  var uploader = client.uploadDir(params);
+  uploader.on('error', function(err) {
+    console.error("unable to sync:", err.stack);
+  });
+  uploader.on('progress', function() {
+    console.log("progress", uploader.progressAmount, uploader.progressTotal);
+  });
+  uploader.on('end', function() {
+    console.log("done uploading");
+  });
+
+}
 
 var ora = require('ora')
 var path = require('path')
@@ -72,14 +87,5 @@ webpack(webpackConfig, function (err, stats) {
     '  Opening index.html over file:// won\'t work.\n'
   ))
 
-  var uploader = client.uploadDir(params);
-  uploader.on('error', function(err) {
-    console.error("unable to sync:", err.stack);
-  });
-  uploader.on('progress', function() {
-    console.log("progress", uploader.progressAmount, uploader.progressTotal);
-  });
-  uploader.on('end', function() {
-    console.log("done uploading");
-  });
+  uploadOnS3();
 })
